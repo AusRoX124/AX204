addb6f21515818e8798659eee1f43831a54e936b
AusRoX124 <austin.schrantz@gmail.com>
2016-09-22 16:59:49 +0800

    project 1

---
+++ Lesson1/poo.js
@@ -0,0 +1,32 @@
+alert("Biiiiiiiitch")
+
+var c =document.getElementById("pedutbutter");
+var ctx = c.getContext('2d');
+
+//ctx.moveTo(0,0);
+//ctx.lineTo(100,200);
+//ctx.stroke();
+ctx.fillStyle = "rgb(0,0,150)";
+ctx.fillRect(100,100,100,100);
+ctx.strokeRect(80,80,140,140);
+ctx.clearRect(120,120,60,60);
+
+var c2 =document.getElementById("chineseSpahgetti");
+var ctx2 = c2.getContext('2d');
+ctx2.fillStyle = "rgb(0,0,150)";
+ctx2.fillRect(150,150,150,150);
+ctx2.fillRect(0,0,150,150);
+
+var c3 =document.getElementById("dumplings");
+var ctx3 = c3.getContext('2d');
+ctx3.fillStyle = "rgb(0,0,150)";
+ctx3.fillRect(0,0,300,300);
+ctx3.clearRect(20,20,120,120);
+ctx3.clearRect(160,20,120,120);
+ctx3.clearRect(20,160,120,120);
+ctx3.clearRect(160,160,120,120);
+
+
+
+
+
